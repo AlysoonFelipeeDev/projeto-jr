64ab5268fe1553a6763731de43216860d6906815
jean <alysoonfelipe@hotmail.com>
2026-02-18 16:29:48 -0300

    feat: cria layout da página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import Login from "./components/Login"
+
 function App() {
   return (
-    <h1>APP</h1>
+    <Login />
   )
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,58 @@
+import styled from "styled-components"
+
 export default function Login(){
-    return <h1>LOGIN</h1>
+    return (
+        <>
+            <header>
+                <Title>Lista de Tarefas</Title>
+            </header>
+            <Form>
+                <input 
+                type="email"
+                placeholder="email" 
+                />
+                <input 
+                type="password"
+                placeholder="senha" 
+                />
+                <ContainerButton>
+                    <Enter type="submit">Entrar</Enter>
+                </ContainerButton>
+            </Form>
+        </>
+    )
 }
+
+const Title = styled.h1`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    input {
+        margin: 0 20px 10px 20px;
+        padding: 8px;
+        border-radius: 5px;
+        border: 2px solid #cacaca;
+    }
+`
+
+const Enter = styled.button`
+    border-radius: 5px ;
+    border: none;
+    width: 50%;
+    padding: 7px;
+    background-color: #5e5efc;
+    color: #ffff;
+    font-size: 15px;
+    cursor: pointer;
+`
+
+const ContainerButton = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
