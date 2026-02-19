9282881551e10a066a57e4605fea6a47b7fca5f4
jean <alysoonfelipe@hotmail.com>
2026-02-18 22:08:35 -0300

    feat: cria página de adicionar Tarefa

--- src/components/CreateTasks.tsx
+++ src/components/CreateTasks.tsx
@@ -1,3 +1,47 @@
+import styled from "styled-components"
+
 export default function CreateTasks() {
-    return <h1>CREATETASKS</h1>
+    return (
+        <ContainerTask>
+            <Header>
+                <h1>Adicionar Tarefas</h1>
+                <button>+</button>
+            </Header>
+            <Form>
+                <input
+                    type="text"
+                    placeholder="Adicione sua tarefa"
+                />
+            </Form>
+        </ContainerTask>
+    )
 }
+
+const ContainerTask = styled.div`
+    width: 100%;
+`
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h1 {
+        margin: 20px 20px;
+        font-size: 3em
+    } 
+    button {
+        margin: 20px 20px;
+        font-size: 20px;
+    }
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    input {
+        margin: 0 20px 0 20px;
+        padding: 6px;
+        border-radius: 5px;
+        border: 2px solid ;
+    }
+`
